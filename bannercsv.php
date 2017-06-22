ac3ba96103a2256864a43ccd9ada272222a7c3b8
Joe Bacal <bacalj@gmail.com>
2017-06-22 12:13:31 -0400

    refactor into a class

--- bannercsv.php
+++ bannercsv.php
@@ -10,6 +10,7 @@ class BannerCsv {
 		$params = array('theid' => $this->courseobj->id);
 		$sql = "SELECT idnumber FROM {course} WHERE id = :theid";
 		$this->crnterm = key($DB->get_records_sql($sql, $params));
+		$this->errorText = 'No errors detected.';
 	}
 
 	function setup_page_and_access(){
@@ -22,8 +23,65 @@ class BannerCsv {
 		$PAGE->set_heading($this->courseobj->fullname);
 	}
 
-	function see_this_obj(){
-		var_dump($this);
+	function collect_student_id_location(){
+		//get name of student id source field from settings
+	}
+
+	function extract_term_and_crn(){
+		$localconfig = get_config('report_bannercsv');
+		$dot_or_hyphen = $localconfig->course_id_pattern;
+		$idnumber_elements = explode($dot_or_hyphen, $this->crnterm);
+
+		if (sizeof($idnumber_elements) > 1 ){
+		  $this->crn = $idnumber_elements[0];
+		  $this->termcode = $idnumber_elements[1];
+		}
+
+		else {
+		  $this->errorText = "It looks like your CourseID doesn't have the right format...";
+		}
+	}
+
+	function display_record_preview(){
+		var_dump($this->crn);
+		var_dump($this->termcode);
+	}
+
+	function build_student_records_stream_content(){
+		$this->students_list = array('Term%20Code%2CCRN%2CStudent%20ID%2CCourse%2CFinal%20Grade%0A');
+
+		foreach ( $this->userlist as $person ) {
+			//each row in csv is going to be a record array
+			$student_record = array();
+
+			//get the students final grade
+			$studentkey = $person->id;
+
+
+			$final_grade_obj = grade_get_course_grades($this->courseobj->id, $studentkey);
+			$final_grade_num = $final_grade_obj->grades[$studentkey]->grade;
+			$final_grade_ltr = $final_grade_obj->grades[$studentkey]->str_grade;
+
+			//build up the record
+			array_push($student_record, $this->termcode);
+			array_push($student_record, $this->crn);
+			array_push($student_record, $person->idnumber);
+			array_push($student_record, $this->courseobj->shortname);
+			array_push($student_record, $final_grade_ltr);
+			//compact each record into a comma-separated string
+			$record_string = implode('%2C', $student_record);
+			//put the record in the records list
+			array_push($this->students_list, $record_string . '%0A');
+		}
+	}
+
+	function render_csv_download_link(){
+		$open_csv_link = '<a href="';
+		$streamer = 'data:application/octet-stream,';
+		$records_as_string = implode('', $this->students_list);
+		$file_name = 'final_grades_' . $this->courseobj->shortname . '_' . date("Y_m_d_His") . '.csv';
+		$close_csv_link = '" download="'. $file_name .'">Download CSV of Final Grades</a>';
+		echo $open_csv_link . $streamer . $records_as_string . $close_csv_link;
 	}
 
 }
--- index.php
+++ index.php
@@ -13,9 +13,17 @@ $bcsv = new BannerCsv($id);
 
 $bcsv->setup_page_and_access();
 
+$bcsv->extract_term_and_crn();
+
 //render
 echo $OUTPUT->header();
 echo '<pre>';
-  $bcsv->see_this_obj();
+  //$bcsv->display_record_preview();
+$bcsv->build_student_records_stream_content();
+$bcsv->display_record_preview();
+$bcsv->fooba();
+$bcsv->render_csv_download_link();
+
+  //$bcsv->collect_student_id_location();
 echo '</pre>';
 echo $OUTPUT->footer();
